b2fd74fab14c53afdadd04b0c1ad1cf57c4f4222
Muhammad Attiq <muhammad.attiq.computer.engineer@gmail.com>
2026-01-29 15:30:56 +0500

    Create Group_Anagrams.py

---
+++ Codes/Group_Anagrams.py
@@ -0,0 +1,13 @@
+# Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        groups = {}
+
+        for word in strs:
+            key = ''.join(sorted(word))
+            if key not in groups:
+                groups[key] = []
+            groups[key].append(word)
+
+        return list(groups.values())
